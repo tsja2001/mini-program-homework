4acabf7a8d0dfd2b9613f8b709ab23a1875e165b
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-03-31 14:16:37 +0800

    feat: 完成初次提交

---
+++ data/questionList.js
@@ -0,0 +1,217 @@
+export const getQuestionList = () => {
+  return [
+    {
+      question: "1. JavaScript中,以下哪个关键字不能用于声明变量？",
+      answer: [
+        {
+          content: "A. let",
+          isRight: false,
+        },
+        {
+          content: "B. var",
+          isRight: false,
+        },
+        {
+          content: "C. const",
+          isRight: false,
+        },
+        {
+          content: "D. constvar",
+          isRight: true,
+        },
+      ],
+    },
+    {
+      question: "2. 在JavaScript中,以下哪个函数用于输出内容到控制台？",
+      answer: [
+        {
+          content: "A. console.log()",
+          isRight: true,
+        },
+        {
+          content: "B. print()",
+          isRight: false,
+        },
+        {
+          content: "C. write()",
+          isRight: false,
+        },
+        {
+          content: "D. println()",
+          isRight: false,
+        },
+      ],
+    },
+    {
+      question: "3. JavaScript中,以下哪个运算符用于比较两个值是否相等？",
+      answer: [
+        {
+          content: "A. ===",
+          isRight: true,
+        },
+        {
+          content: "B. ==",
+          isRight: false,
+        },
+        {
+          content: "C. =",
+          isRight: false,
+        },
+        {
+          content: "D. !==",
+          isRight: false,
+        },
+      ],
+    },
+    {
+      question:
+        "4. 在JavaScript中,以下哪个数组方法可以将新元素添加到数组的末尾？",
+      answer: [
+        {
+          content: "A. push()",
+          isRight: true,
+        },
+        {
+          content: "B. pop()",
+          isRight: false,
+        },
+        {
+          content: "C. shift()",
+          isRight: false,
+        },
+        {
+          content: "D. unshift()",
+          isRight: false,
+        },
+      ],
+    },
+    {
+      question:
+        "5. 在JavaScript中,以下哪个对象属性可以用于获取URL中的查询参数？",
+      answer: [
+        {
+          content: "A. location.pathname",
+          isRight: false,
+        },
+        {
+          content: "B. location.host",
+          isRight: false,
+        },
+        {
+          content: "C. location.search",
+          isRight: true,
+        },
+        {
+          content: "D. location.protocol",
+          isRight: false,
+        },
+      ],
+    },
+    {
+      question: "6. JavaScript中,以下哪个方法可以将字符串转换为数字？",
+      answer: [
+        {
+          content: "A. Object.key()",
+          isRight: false,
+        },
+        {
+          content: "B. toString()",
+          isRight: false,
+        },
+        {
+          content: "C. parseFloat()",
+          isRight: true,
+        },
+        {
+          content: "D. toFixed()",
+          isRight: false,
+        },
+      ],
+    },
+    {
+      question: "7. 以下哪个不是JavaScript的数据类型？",
+      answer: [
+        {
+          content: "A. Number",
+          isRight: false,
+        },
+        {
+          content: "B. String",
+          isRight: false,
+        },
+        {
+          content: "C. Boolean",
+          isRight: false,
+        },
+        {
+          content: "D. Array",
+          isRight: true,
+        },
+      ],
+    },
+    {
+      question:
+        "8. 在JavaScript中,以下哪个事件会在用户单击页面上的元素时触发？",
+      answer: [
+        {
+          content: "A. onload",
+          isRight: false,
+        },
+        {
+          content: "B. onsubmit",
+          isRight: false,
+        },
+        {
+          content: "C. onclick",
+          isRight: true,
+        },
+        {
+          content: "D. onchange",
+          isRight: false,
+        },
+      ],
+    },
+    {
+      question: "9. JavaScript中,以下哪个方法可以用于获取对象的属性名称？",
+      answer: [
+        {
+          content: "A. Object.keys()",
+          isRight: true,
+        },
+        {
+          content: "B. Object.values()",
+          isRight: false,
+        },
+        {
+          content: "C. Object.entries()",
+          isRight: false,
+        },
+        {
+          content: "D. Object.value()",
+          isRight: false,
+        },
+      ],
+    },
+    {
+      question: "10. 在JavaScript中,以下哪个方法可以用于删除对象的属性？",
+      answer: [
+        {
+          content: "A. deleteProperty()",
+          isRight: false,
+        },
+        {
+          content: "B. removeProperty()",
+          isRight: false,
+        },
+        {
+          content: "C. delete()",
+          isRight: true,
+        },
+        {
+          content: "D. remove()",
+          isRight: false,
+        },
+      ],
+    },
+  ];
+};
---
+++ pages/test/test.js
@@ -0,0 +1,98 @@
+import { getQuestionList } from "../../data/questionList";
+
+// pages/test/test.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 题目列表
+    questionList: [],
+    // 记录当前题目的索引
+    currentIndex: 0,
+    // 记录用户选择的答案
+    userAnswer: [],
+    // 当前选择题目的索引
+    currentChangedIndex: -1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.setData({
+      questionList: getQuestionList(),
+    });
+  },
+
+  // 点击题目选项的事件处理函数
+  tapHandler(e) {
+    const { value } = e.currentTarget.dataset;
+
+    const { currentIndex, userAnswer } = this.data;
+
+    userAnswer[currentIndex] = value;
+
+    this.setData({
+      userAnswer,
+    });
+  },
+
+  // 点击下一题的事件处理函数
+  nextHandler(){
+    if(this.data.currentIndex === this.data.questionList.length - 1){
+      wx.navigateTo({
+        url: '/pages/result/result',
+      });
+
+      wx.setStorage({
+        key: 'userAnswer',
+        data: this.data.userAnswer,
+      });
+    }
+    this.setData({
+      currentIndex: this.data.currentIndex + 1,
+    });
+  },
+  // 
+  prevHandler(){
+    this.setData({
+      currentIndex: this.data.currentIndex - 1,
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
