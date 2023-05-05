e39942ad415a3e2bdcbeb2f68ac29599b7d672bc
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-05-05 08:32:59 +0800

    feat: 新建第三次作业分支

--- app.js
+++
@@ -1,9 +0,0 @@
-import { mookFormData } from "./mook/index";
-import Store from "./store/index";
-
-App({
-  // 全局状态管理
-  store: new Store({
-    formData: mookFormData,
-  }),
-});
--- pages/logs/logs.js
+++
@@ -1,8 +0,0 @@
-
-Page({
-  data: {
-    logs: []
-  },
-  onLoad() {
-  }
-})
